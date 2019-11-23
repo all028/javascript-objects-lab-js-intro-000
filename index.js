0c1680e1c2f8f9ae9a5815a1192219b1dd0dbf29
al <albliu@email.com>
2019-11-23 03:53:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
   return obj;
 }
 
-function deleteFromObjectByKey(obj, key, value){
-
-  return obj;
+function deleteFromObjectByKey(obj, key){
+  var newObj = Object.assign({}, obj)
+  delete newObj.key;
+  return newObj;
+  
 }
 
 
